5fefbdfecb783381e4f0d2bcf8ba71f2a32c4f3e
Sotatek-HaiNguyen4 <hai.nguyen4@sotatek.com>
2025-07-22 20:03:37 +0700

    fix: add env client

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     Credential: true,
   });
 
